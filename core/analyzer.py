2ced9349580c73be047311f2424e315849862009
Zem-dev8 <danbajikila59@gmail.com>
2026-05-09 19:08:50 +0100

    Update analyzer.py

--- core/analyzer.py
+++ core/analyzer.py
@@ -1,1 +1,16 @@
-.
+import json
+
+def charger_base_signatures(chemin_db="database/signatures.json"):
+    """Charge les signatures depuis le fichier JSON."""
+    try:
+        with open(chemin_db, "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print("⚠️ Base de données introuvable, création d'une base vide.")
+        return {}
+
+def verifier_menace(hash_fichier, base_signatures):
+    """Vérifie si un hash est présent dans la base de données."""
+    if hash_fichier in base_signatures:
+        return base_signatures[hash_fichier]  # Retourne le nom du virus
+    return None
